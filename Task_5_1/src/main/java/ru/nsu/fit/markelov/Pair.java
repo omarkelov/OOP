125174996b415f2160d2d2fcc0695280cb709cde
omarkelov <36790281+omarkelov@users.noreply.github.com>
2019-11-22 15:10:30 +0700

    0.11

---
+++ Task_5_1/src/main/java/ru/nsu/fit/markelov/Pair.java
@@ -0,0 +1,52 @@
+package ru.nsu.fit.markelov;
+
+public class Pair<K extends Comparable, V> implements Comparable<Pair> {
+
+    private K key;
+    private V value;
+
+    /**
+     * Creates a Pair that represents a <(key, value)> object.
+     * <p>
+     * The key must be of Comparable type as it is used to implement
+     * the Comparable interface.
+     *
+     * @param key   the key
+     * @param value the value
+     */
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public int compareTo(Pair anotherPair) {
+        return key.compareTo(anotherPair.getKey());
+    }
+
+    /**
+     * @return the pair as <(key, value)> string
+     */
+    @Override
+    public String toString() {
+        return key + "(" + value + ")";
+    }
+
+    /**
+     * return the key of this pair
+     */
+    public K getKey() {
+        return key;
+    }
+
+    /**
+     * return the value of this pair
+     */
+    public V getValue() {
+        return value;
+    }
+}
--- Task_5_1/src/test/java/ru/nsu/fit/markelov/Test.java
+++ Task_5_1/src/test/java/ru/nsu/fit/markelov/Test.java
@@ -2,21 +2,58 @@ package ru.nsu.fit.markelov;
 
 import org.junit.Assert;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Test {
+
+    private final ArrayList<Pair<Integer, Integer>> list = new ArrayList<>();
+
+    private final class Task implements Runnable {
+
+        private List<Integer> sublist;
+        private int id;
+
+        public Task(List<Integer> sublist, int id) {
+            this.sublist = sublist;
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            for (Integer elem : sublist) {
+                synchronized (list) {
+                    list.add(new Pair<>(elem, id));
+                }
+            }
+        }
+    }
 
     @org.junit.Test
     public void test() {
         long startNanoTime;
         boolean nonPrimeFound;
-        ArrayList<Integer> numbers = InputArrayGenerator.generate(10000, 1000, new Random(5));
+        ArrayList<Integer> numbers = InputArrayGenerator.generate(10000, 1, new Random(5));
+
+        Task task1 = new Task(numbers.subList(0, numbers.size() / 2), 1);
+        Task task2 = new Task(numbers.subList(numbers.size() / 2, numbers.size()), 2);
+        Thread thread1 = new Thread(task1);
+        Thread thread2 = new Thread(task2);
+        thread1.start();
+        thread2.start();
+
+        try {
+            wait(1000);
+            System.out.println(list);
+        } catch (InterruptedException e) {
+
+        }
+
 
         // -----------------------------------------------------------------------------------------
 
-        startNanoTime = System.nanoTime();
+        /*startNanoTime = System.nanoTime();
 
         nonPrimeFound = false;
         for (Integer number : numbers) {
@@ -34,7 +71,8 @@ public class Test {
 
         startNanoTime = System.nanoTime();
 
-        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+//        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+        ThreadPool threadPool = new ThreadPool(5);
 
         ArrayList<Future<Boolean>> futures = new ArrayList<>();
         for (Integer number : numbers) {
@@ -57,10 +95,10 @@ public class Test {
 
         Assert.assertTrue(nonPrimeFound);
 
-        System.out.println(System.nanoTime() - startNanoTime);
+        System.out.println(System.nanoTime() - startNanoTime);*/
 
         /*startNanoTime = System.nanoTime();
-        Assert.assertTrue(numbers.stream().anyMatch(PrimeNumbers::isNonPrime));
+        Assert.assertTrue(numbers.parallelStream().anyMatch(PrimeNumbers::isNonPrime));
         System.out.println(System.nanoTime() - startNanoTime);*/
     }
 }
