80b42b649c8ea62a351849599d69485e233133d9
omarkelov <36790281+omarkelov@users.noreply.github.com>
2019-11-30 12:33:29 +0700

    0.91

--- Task_2_3/src/test/java/ru/nsu/fit/markelov/TreeTest.java
+++ Task_2_3/src/test/java/ru/nsu/fit/markelov/TreeTest.java
@@ -3,6 +3,7 @@ package ru.nsu.fit.markelov;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -94,6 +95,23 @@ public class TreeTest {
             iterator.next();
             Assert.fail();
         } catch (NoSuchElementException e) {
+            System.out.println(e.getClass().getSimpleName() + " got caught.");
+        }
+    }
+
+    @Test
+    public void testConcurrentModificationException() {
+        Tree<String> nsu = new Tree<>("NSU");
+        nsu.add("MMF");
+        nsu.add("FF");
+
+        try {
+            for (Tree<String> child : nsu) {
+                System.out.println(child.getValue());
+                nsu.add("FIT");
+            }
+            Assert.fail();
+        } catch (ConcurrentModificationException e) {
             System.out.println(e.getClass().getSimpleName() + " got caught.");
         }
     }
